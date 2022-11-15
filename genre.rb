3f719b5118117d95250e3098224a54ef0c8d9a89
GordonOtieno <otienogordon95@gmail.com>
2022-11-15 12:37:03 +0300

    modify genre class

--- genre.rb
+++ genre.rb
@@ -1,13 +1,12 @@
 class Genre
-    def initialize(name)
-        @id = Random.rand(1..500)
-        @name = name
-        @items = []
-    end
+  def initialize(name)
+    @id = Random.rand(1..500)
+    @name = name
+    @items = []
+  end
 
-    def add_item(item)
-        @items << item
-        puts @items
-    end
-
+  def add_item(item)
+    @items << item
+    puts @items
+  end
 end
--- music_album/misic_album.rb
+++ music_album/misic_album.rb
@@ -1,15 +1,14 @@
 require_relative '../item'
 
 class MusicAlbum < Item
-    attr_accessor :on_spotify
-    
-    def initialize (on_spotify)
-        @on_spotify= on_spotify
-        super(publish_date)
-    end
-    
-    def can_be_archived?
-     super && @on_spotify
-    end
+  attr_accessor :on_spotify
 
+  def initialize(on_spotify)
+    @on_spotify = on_spotify
+    super(publish_date)
+  end
+
+  def can_be_archived?
+    super && @on_spotify
+  end
 end
