9d1b537fb39965380d41cc0b33bdd745b37bf689
Haadiiii <hammali99@gmail.com>
2022-11-16 19:32:04 +0500

    resolved linters

--- book/book_module.rb
+++ book/book_module.rb
@@ -17,9 +17,9 @@ module BookModule
     author = Author.new(first_name, last_name)
     label = Label.new(title, color)
     book = Book.new(publisher, cover_state, publish_date)
-    puts "book #{book} added"
+    puts 'book added successfully'
     json = JSON.generate(ItemStruct.new({ publisher: publisher, cover_state: cover_state,
-                                          publish_date: publish_date, label: label.title, color: label.color, author: author.first_name, author: author.last_name }))
+                                          publish_date: publish_date, label: label.title, color: label.color, first_name: author.first_name, last_name: author.last_name }))
     @books << json
     File.write('./book/books.json', @books)
   end
@@ -32,7 +32,7 @@ module BookModule
     else
       @books.each_with_index do |s, index|
         s = JSON.parse(s, create_additions: true)
-        puts "#{index + 1}. #{s.item['label']}"
+        puts "#{index + 1}). #{s.item['label']}"
       end
     end
   end
@@ -45,9 +45,9 @@ module BookModule
     else
       # Read file from ./book/books.json
       puts "\nList of all Books"
-      @books.each_with_index do |book, _index|
+      @books.each_with_index do |book, index|
         book = JSON.parse(book, create_additions: true)
-        puts "Publisher: \"#{book.item["publisher"]}\", Cover State: #{book.item["cover_state"]}, Publish Date: \"#{book.item["publish_date"]}\", Label: #{book.item["label"]}, Color: #{book.item["color"]}, First name: #{book.item["first_name"]}, Last Name: #{book.item["last_name"]}"
+        puts "#{index + 1}). Publisher: \"#{book.item["publisher"]}\", Cover State: #{book.item["cover_state"]}, Publish Date: \"#{book.item["publish_date"]}\", Label: #{book.item["label"]}, Color: #{book.item["color"]}, First name: #{book.item["first_name"]}, Last Name: #{book.item["last_name"]}"
       end
     end
   end
@@ -73,9 +73,9 @@ module BookModule
     if @books.empty?
       puts "\nBook list is empty"
     else
-      @books.each_with_index do |book, _index|
+      @books.each_with_index do |book, index|
         book = JSON.parse(book, create_additions: true)
-        puts "#{book.item['author']} authored \"#{book.item['label']}\"."
+        puts "#{index + 1}). #{book.item['first_name']} authored \"#{book.item['label']}\"."
       end
     end
   end
